bede1509d9ae8afadce2e0972f072e18ff04b684
NewtonGain <newtongain7@gmail.com>
2021-12-23 18:37:49 +0600

    yuuyt

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(MyApp());
+void main() => runApp(MyApp());
+
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -10,106 +11,122 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   double _currentSliderValue = 0;
-   int _counter = 0;
-   void _increament(){
-     setState(() {
-       _counter++;
-     });
-   }
-   void _decreament(){
-     setState(() {
-       _counter--;
-     });
-   }
-    var activeCardColor = Colors.amber;
+  int _counter = 0;
+  void _increament() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decreament() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  var activeCardColor = Colors.amber;
   var inActiveCardColor = Color(0xFF272B4E);
 
-  var maleCardColor = Color(0xFF272B4E);
+  var firstContainer = Color(0xFF272B4E);
 
-  var femaleCardColor = Color(0xFF272B4E);
+  var secondContainer = Color(0xFF272B4E);
 
-  void updateColor(int gender) {
-    if (gender == 1) {
-      if (maleCardColor == inActiveCardColor) {
-        maleCardColor = activeCardColor;
-        femaleCardColor = inActiveCardColor;
-      } else if (maleCardColor == activeCardColor) {
-        maleCardColor = inActiveCardColor;
+  void updateColor(int colorchange) {
+    if (colorchange == 1) {
+      if (firstContainer == inActiveCardColor) {
+        firstContainer = activeCardColor;
+        secondContainer = inActiveCardColor;
+      } else if (firstContainer == activeCardColor) {
+        firstContainer = inActiveCardColor;
       }
     }
   }
 
-  void updateColor2(int gender) {
-    if (gender == 2) {
-      if (femaleCardColor == inActiveCardColor) {
-        femaleCardColor = activeCardColor;
-        maleCardColor = inActiveCardColor;
-      } else if (femaleCardColor == activeCardColor)
-        femaleCardColor = inActiveCardColor;
+  void updateColor2(int colorchange) {
+    if (colorchange == 2) {
+      if (secondContainer == inActiveCardColor) {
+        secondContainer = activeCardColor;
+        firstContainer = inActiveCardColor;
+      } else if (secondContainer == activeCardColor)
+        secondContainer = inActiveCardColor;
     }
   }
 
-   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(appBar: 
-    AppBar(
-      title: Text('Exam Test'),
-      centerTitle: true,
-      ),
-      body: Column(children: [
-        Expanded(child: Row(children: [
-         
-          Expanded(child: Card(
-            color: Colors.teal,
-            child: Column(children: [
-              Text('$_counter',style: TextStyle(fontSize: 66),),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:  [
-                
-                FloatingActionButton(onPressed: _increament, child: Icon(Icons.add), ),
-                 FloatingActionButton(onPressed: _decreament, child: Icon(Icons.remove), )
-              ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Exam Test'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Card(
+                      color: Colors.teal,
+                      child: Column(
+                        children: [
+                          Text(
+                            '$_counter',
+                            style: TextStyle(fontSize: 66),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: _increament,
+                                child: Icon(Icons.add),
+                              ),
+                              FloatingActionButton(
+                                onPressed: _decreament,
+                                child: Icon(Icons.remove),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
             ),
-          ),
-          ),
-        
-        ],
-        ),
-        
-        ),
-        Expanded(child: Card(
-          color: Colors.pink,
-          child: Row(children: [
-            Expanded(child: Column(children: [
-              Text(_currentSliderValue.round().toString(),style: TextStyle(fontSize: 89),),
-              
-        
-              Slider(
-              value: _currentSliderValue,
-              max: 200,
-              activeColor: Colors.red,
-             inactiveColor: Colors.white,
-        
-              
-              onChanged: ( double value) {
-          setState(() {
-            _currentSliderValue = value;
-          });
-              },
-            )
-            ],)),
-            
-          ],
-          ),
-        ),
-        
-        ),
-        Expanded(child: Row(children: [
-          Expanded(
+            Expanded(
+              child: Card(
+                color: Colors.pink,
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Column(
+                      children: [
+                        Text(
+                          _currentSliderValue.round().toString(),
+                          style: TextStyle(fontSize: 89),
+                        ),
+                        Slider(
+                          value: _currentSliderValue,
+                          max: 200,
+                          activeColor: Colors.red,
+                          inactiveColor: Colors.white,
+                          onChanged: (double value) {
+                            setState(() {
+                              _currentSliderValue = value;
+                            });
+                          },
+                        )
+                      ],
+                    )),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
                     child: GestureDetector(
                       onTap: () {
                         setState(() {
@@ -117,31 +134,31 @@ class _MyAppState extends State<MyApp> {
                         });
                       },
                       child: Container(
-                        color: maleCardColor,
-                        
+                        color: firstContainer,
                       ),
                     ),
+                  ),
+                  SizedBox(
+                    width: 4,
                   ),
                   Expanded(
                     child: GestureDetector(
                       onTap: () {
                         setState(() {
-                          updateColor(2);
+                          updateColor2(2);
                         });
                       },
                       child: Container(
-                        color: femaleCardColor,
-                        
+                        color: secondContainer,
                       ),
                     ),
                   ),
-         
-        ],
+                ],
+              ),
+            ),
+          ],
         ),
-        
-        ),
-      ],),
       ),
-      );
+    );
   }
 }
